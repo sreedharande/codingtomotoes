77d2336b5de5c47a1497c81566fda2e5ecaf53ad
Sreedhar Ande <andesreedhar@gmail.com>
2018-06-10 18:49:39 -0700

    LongestCommonPrefix

---
+++ LongestCommonPrefix/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestCommonPrefix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strs = { "flower", "flow", "flight" };
+
+            string prefix = strs[0];
+
+            for(int i=1;i<strs.Length;i++)
+            {
+                while(strs[i].IndexOf(prefix) != 0)
+                {
+                    if(!string.IsNullOrEmpty(prefix))
+                        prefix = prefix.Substring(0, prefix.Length - 1);
+                }
+            }
+            Console.WriteLine(prefix);
+        }
+    }
+}
